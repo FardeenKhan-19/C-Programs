7bbf779a915f090bf68cbe02878be42148616da4
Fardeen Khan <khanfardeen19.ka@gmail.com>
2024-09-23 11:01:36 +0530

    Add files via upload

---
+++ Fardeen Khan/Program1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/*
+
+Experiment
+
+Title:
+
+Name    : Fardeen Firoz Khan
+
+Division: FE(CE) D
+
+UIN     : 241P096
+
+*/
+
+int main()
+{
+
+
+return 0;
+}
+/*
+
+Output:
+
+
+
+
+*/
